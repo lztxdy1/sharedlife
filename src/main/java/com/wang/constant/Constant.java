6b2cd51f85ae60d85ef494ed92e844d6345fadfb
halo <980237501@qq.com>
2023-02-14 21:05:00 +0800

    全部完成

--- src/main/java/com/wang/constant/Constant.java
+++ src/main/java/com/wang/constant/Constant.java
@@ -1,5 +1,8 @@
 package com.wang.constant;
 
+/**
+ * @author laptop
+ */
 public class Constant {
     public static final String LOGIN_ERROR_INFO = "用户名或者密码错误!";
     public static final String DATE_FORMAT = "yyyy-MM-DD HH:mm:ss";
